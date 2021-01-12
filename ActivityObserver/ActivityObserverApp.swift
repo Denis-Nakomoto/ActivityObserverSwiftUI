528d3a2006d6597ad6c168fed1d1f9b99ffb68a3
Denis <Svetlakov>
2021-01-12 17:14:55 +1000

    Initial Commit

---
+++ ActivityObserver/ActivityObserverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityObserverApp.swift
+//  ActivityObserver
+//
+//  Created by Denis Svetlakov on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct ActivityObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
